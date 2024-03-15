2d6504ed33077ddc819ea38e3c302553653e1696
rumeethundal <rumeethundal03@gmail.com>
2024-03-14 19:02:17 -0700

    created function to calculate regular price

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scan = new Scanner (System.in);
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Gasoline Price Calculator!\n" +
-                "Please choose which type of gasoline your vehicle takes:\n" +
-                "Regular, Plus, Preimium, or Diesel: ");
-        Scanner scan = new Scanner (System.in);
+                "Please choose which type of gasoline your vehicle takes\n" +
+                "Regular, Plus, Premium, or Diesel: ");
         String input = scan.nextLine();
+
         while(true) {
             switch(input) {
                 case "Regular":
+                    calculatePriceOfRegular();
                     break;
+
                 case "Plus":
                     break;
+
                 case "Preimium":
                     break;
+
                 case "Diesel":
                     break;
             }
         }
+    }
+    public static void calculatePriceOfRegular() {
+        double regularPricePerGallon = 4.879;
+        System.out.println("How many gallon do you need?");
+        int gallonsOfRegularNeeded = scan.nextInt();
+        double totalPriceOfRegular = regularPricePerGallon * gallonsOfRegularNeeded;
+        System.out.println("Your total price is: " + "$" + totalPriceOfRegular);
     }
 }
